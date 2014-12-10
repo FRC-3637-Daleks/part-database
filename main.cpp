c3374e5970bfb824eb144ffb39ed6ac7e68d9c98
Elvin Kong <ekong1@hcrhs.org>
2014-12-10 18:15:15 -0500

    Main File
    This is the main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: ekong
+ *
+ * Created on December 10, 2014, 6:11 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
